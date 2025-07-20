d0562742b10965dc24f647b65eafd1793e2dccf0
Andre Pretorius <andrepret4@icloud.com>
2025-07-20 20:48:47 +0200

    Fixed hero image position issue and customized

--- ajp-portfolio/app/components/HeroSection.jsx
+++ ajp-portfolio/app/components/HeroSection.jsx
@@ -10,19 +10,19 @@ const HeroSection = () => {
             <div className="grid grid-cols-1 lg:grid-cols-12">
                 <div className="col-span-7 place-self-center text-center sm:text-left">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600" >
-                            Hello, I'm{" "}
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-300" >
+                            Hi, I'm{" "}
                         </span>
                         <br></br>
                         <TypeAnimation
                             sequence={[
                                 'Andre',
                                 1000,
-                                'Problem Solver',
+                                'A Problem Solver',
                                 1000,
-                                'Backend Engineer',
+                                'A Backend Engineer',
                                 1000,
-                                'Innovator',
+                                'An Innovator',
                                 1000
                             ]}
                             wrapper="span"
@@ -35,19 +35,19 @@ const HeroSection = () => {
                         My Description
                     </p>
                     <div>
-                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-blue-900 hover:bg-slate-200 text-white">
                             Hire Me
                         </button>
-                        <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+                        <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-blue-900 hover:bg-slate-800 text-white mt-3">
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
                         </button>
                     </div>
                 </div>
 
                 <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]">
+                    <div className="relative rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]">
                         <Image
-                            src="/hero-image.png"
+                            src="/hero-image-2.png"
                             alt="hero image"
                             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             width={300}

0ad867d5fc001e7c75f2b9a3a793e8d96692588d
Andre Pretorius <andrepret4@icloud.com>
2025-08-13 21:43:02 +0200

    Added Projects section

---
+++ ajp-portfolio/app/components/ProjectsSection.jsx
@@ -0,0 +1,59 @@
+"use client";
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "ClassMate",
+    description: "ClassMate Description",
+    image: "/classmate.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/"
+  },
+  {
+    id: 2,
+    title: "NWU Human Movement Sciences Management Platform",
+    description: "NWU HMS Description",
+    image: "",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/"
+  }
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) => project.tag.includes(tag));
+
+  return (
+    <>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-4">My Projects</h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag onClick={handleTagChange} name="All" isSelected={tag === "All"} />
+        <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web"} />
+      </div>
+      <div>
+        {filteredProjects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProjectsSection;
--- ajp-portfolio/app/page.js
+++ ajp-portfolio/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
+        <ProjectsSection />
       </div>
     </main>
   );

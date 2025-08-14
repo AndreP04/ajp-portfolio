61d9d88803a02256f253407054640e5a7da5b50f
AndreP04 <andrepret4@icloud.com>
2025-08-14 12:44:36 +0200

    Added animations using framer motion

--- ajp-portfolio/app/components/Navbar.jsx
+++ ajp-portfolio/app/components/Navbar.jsx
@@ -23,10 +23,10 @@ const navLinks = [
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] opacity-100">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] opacity-100">
+      <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
-          LOGO
+          AJP
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
@@ -46,7 +46,7 @@ const Navbar = () => {
           )}
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:sapce-x-8 mt-0">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
               <li key={index}>
                 <NavLink href={link.path} title={link.title}></NavLink>

2a5db2454ffcce207c44c7b60f13585d1cb74f11
Andre Pretorius <andrepret4@icloud.com>
2025-08-18 19:09:03 +0200

    Add CV to download button

--- ajp-portfolio/app/components/HeroSection.jsx
+++ ajp-portfolio/app/components/HeroSection.jsx
@@ -33,9 +33,11 @@ const HeroSection = () => {
             >
               Contact Me
             </button>
-            <button className="cursor-pointer px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-blue-900 hover:bg-slate-800 text-white mt-3">
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
-            </button>
+            <a href="/Andre Pretorius - Junior Systems Engineer - CV.pdf">
+              <button className="cursor-pointer px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-blue-900 hover:bg-slate-800 text-white mt-3">
+                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+              </button>
+            </a>
           </div>
         </motion.div>
 

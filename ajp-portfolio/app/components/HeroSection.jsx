98ece6e8dad4ff4d765c60de9ccdfab204e0ce70
Andre Pretorius <andrepret4@icloud.com>
2025-08-29 05:57:54 +0200

    Upload updated  CV and hero image

--- ajp-portfolio/app/components/HeroSection.jsx
+++ ajp-portfolio/app/components/HeroSection.jsx
@@ -49,7 +49,7 @@ const HeroSection = () => {
         >
           <div className="relative rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px]">
             <Image
-              src="/main-image.jpg"
+              src="/hero-image-2.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 scale-130"
               width={300}

d5ffee16a9d53b621b5d77f2f5592c1c30f6522a
AndreP04 <andrepret4@icloud.com>
2026-03-29 21:28:54 +0200

    refactor: remove quote

--- ajp-portfolio/app/components/HeroSection.jsx
+++ ajp-portfolio/app/components/HeroSection.jsx
@@ -25,7 +25,6 @@ const HeroSection = () => {
             />
           </h1>
 
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">// yes, I do both.</p>
           <div>
             <button
               onClick={() => document.getElementById("contact").scrollIntoView({ behavior: "smooth" })}

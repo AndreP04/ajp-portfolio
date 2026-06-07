f17fd7f7cc846c8d76340e802ae5ce43a103645b
AndreP04 <andrepret4@icloud.com>
2026-06-07 21:30:44 +0200

    feat: add new project

--- ajp-portfolio/app/components/ProjectsSection.jsx
+++ ajp-portfolio/app/components/ProjectsSection.jsx
@@ -19,10 +19,20 @@ const projectsData = [
     id: 2,
     title: "NWU Human Movement Sciences Management Platform",
     description:
-      "The NWU Human Movement Sciences Digital Platform is an integrated solution designed to enhance the accessibility and engagement of Human Movement Sciences resources at North West University. This project includes both a website and mobile application, aimed at providing students and faculty with a seamless experience to access information, resources, and tools related to the School of Human Movement Sciences.",
+      "The NWU Human Movement Sciences Digital Platform is an integrated solution designed to enhance the accessibility and engagement of Human Movement Sciences resources at North West University.",
     image: "/hms.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/AndreP04/HumanMovementSciences-NWU",
+    previewUrl: "/"
+  },
+  {
+    id: 3,
+    title: "BookMark",
+    description:
+      "Personal reading progress tracker that allows users to add books and log reading sessions to track their progress over time.",
+    image: "/bookmark.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/AndreP04/bookmark",
     previewUrl: "/"
   }
 ];
